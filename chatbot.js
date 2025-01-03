af4ac294c45d581043cc67a1feb36448fb316ecd
Oscar Rezende <netox2002@gmail.com>
2025-01-03 18:32:16 -0300

    feat: Bot menu / First contact with the user

--- chatbot.js
+++ chatbot.js
@@ -14,3 +14,25 @@ client.on('ready', () => {
 client.initialize();
 
 const delay = ms => new Promise(res => setTimeout(res, ms)); // Function we use to create the delay between one action and another
+
+
+// Funnel
+
+client.on('message', async msg => {
+
+  if (msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|Ola|salve|eai|Salve|Eai|Opa|opa)/i) && msg.from.endsWith('@c.us')) {
+
+      const chat = await msg.getChat();
+
+      await delay(3000); // 3 second delay
+      await chat.sendStateTyping(); // Simulating Typing
+      await delay(3000); // Delay of 3000 milliseconds better known as 3 seconds
+      const contact = await msg.getContact(); // Getting the contact user
+      const name = contact.pushname; // Getting the contact name
+      await client.sendMessage(msg.from,'Olá! '+ name.split(" ")[0] + '. Sou o assistente virtual da Rezcode. Como posso ajudá-lo(a) hoje? Por favor, digite uma das opções abaixo:\n\n1 - Como funciona\n2 - Valores dos planos\n3 - Benefícios\n4 - Como aderir\n5 - Outras perguntas'); //Primeira mensagem de texto
+      await delay(3000); //delay of 3 seconds
+      await chat.sendStateTyping(); // Simulating Typing
+      await delay(5000); //delay of 5 seconds
+  
+      
+  }
